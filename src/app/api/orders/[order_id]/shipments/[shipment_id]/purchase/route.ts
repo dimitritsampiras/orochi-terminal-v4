5cb24cbccf5b93c4360ca0e0bf70a1894a0a4339
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2025-12-27 08:42:25 -0500

    changes

--- src/app/api/orders/[order_id]/shipments/[shipment_id]/purchase/route.ts
+++ src/app/api/orders/[order_id]/shipments/[shipment_id]/purchase/route.ts
@@ -10,7 +10,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(
   request: Request,
-  { params }: { params: { order_id: string; shipment_id: string } }
+  { params }: { params: Promise<{ order_id: string; shipment_id: string }> }
 ): Promise<NextResponse<PurchaseShipmentResponse>> {
   const user = await authorizeUser(["superadmin", "admin"]);
 
